5a9c3ff435cdd8031cca1ee3973dabcb02e97614
KMacDonald222 <keeganm742@gmail.com>
2025-04-01 18:46:35 -0300

    Finished preparation for PRNG tests

---
+++ tests/jtest/compile_results.py
@@ -0,0 +1,39 @@
+# File:     compile_results.py
+# Author:   Keegan MacDonald
+# Created:  2025.04.01
+# Purpose:  Extract the pass/fail results of the NIST SP 800-22 Rev 1a tests
+#           and calculate the success rate of each test
+
+# Get the number of test result files
+file_count = int(input("Test count: "))
+
+# Initialize test results
+test_names = [ "" ] * 15
+results = [ 0 ] * 15
+
+# Read all test results
+for index in range(file_count):
+    in_file = open("results/" + str(index + 1) + ".txt", "r")
+    lines = in_file.readlines()
+    in_file.close();
+    # Iterate through test result lines
+    for li in range(len(lines) - 15, len(lines)):
+        p = 15 - (len(lines) - li)
+        line = lines[li].strip()
+        # Get the name of the current test
+        if len(test_names[p]) == 0:
+            for c in line:
+                if c == ' ':
+                    break
+                test_names[p] += c
+        # Add result
+        if line.endswith("PASS"):
+            results[p] += 1
+
+# Print compiled results
+for i in range(15):
+    tn = test_names[i]
+    r = results[i]
+    fc = file_count
+    pc = (100.0 * float(r)) / float(fc)
+    print(tn, ": ", r, " / ", fc, " = ", pc, "%", sep = "")
--- tests/jtest/run_tests.sh
+++ tests/jtest/run_tests.sh
@@ -4,8 +4,11 @@
 # Purpose:  Run the Python implementation of the SP 800-22 Rev 1a Test Suite on
 #           each file in the data/ directory
 
+# Get the number of files to test
+read -p "Data file count: " count
+
 # Iterate over file names
-for index in {1..100}
+for index in $(seq 1 $count)
 do
     # Test the current file
     echo "Testing data/"$index".bin"
