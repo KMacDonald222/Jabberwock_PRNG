ff6b83faff841bc95e338b444447bf476e9e7dd0
KMacDonald222 <keeganm742@gmail.com>
2025-04-01 15:34:34 -0300

    Preparation to generate test data

---
+++ tests/jtest/run_tests.sh
@@ -0,0 +1,14 @@
+# File:     run_tests.sh
+# Author:   Keegan MacDonald
+# Created:  2025.04.01
+# Purpose:  Run the Python implementation of the SP 800-22 Rev 1a Test Suite on
+#           each file in the data/ directory
+
+# Iterate over file names
+for index in {1..100}
+do
+    # Test the current file
+    echo "Testing data/"$index".bin"
+    python3 sp800_22_tests/sp800_22_tests.py "data/"$index".bin" \
+        > "results/"$index".txt"
+done
